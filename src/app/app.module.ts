4b1c201d08b083d01e63fa5c6fb35ed61fa08e9b
Saykee <chrisykelly22@gmail.com>
2019-04-02 23:41:16 +0100

    Some Css Improvements - Password Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PwnedDataComponent } from './pwned-data/pwned-data.component';
-import { specialPipe} from '../app/services/specialPipe'
-
+import { specialPipe} from '../app/services/specialPipe';
+import { PasswordCheckerComponent } from './password-checker/password-checker.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     PwnedDataComponent,
-    specialPipe
+    specialPipe,
+    PasswordCheckerComponent
   ],
   imports: [
     BrowserModule,
